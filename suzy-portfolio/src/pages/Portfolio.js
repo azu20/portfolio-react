23abc943939fbe2ebb574913c10a326d67e16931
Azucena Herrera <azucenaphx@gmail.com>
2020-07-27 09:13:09 -0700

    updated portfolio page

--- suzy-portfolio/src/pages/Portfolio.js
+++ suzy-portfolio/src/pages/Portfolio.js
@@ -4,20 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import CardProjectOne from '../components/Card/CardProjectOne';
 import CardProjectTwo from '../components/Card/CardProjectTwo';
 import CardProjectThree from '../components/Card/CardProjectThree';
-import CardProjectFour from '../components/Card/CardProjectFour';
-import CardProjectFive from '../components/Card/CardProjectFive';
-import CardProjectSix from '../components/Card/CardProjectSix';
-import Footer from '../components/Footer/Footer';
 import "./portfolio.css";
 
 
 
 const Portfolio = () => {
     return (
-     
-      <Container>
-        <h1>Portfolio</h1>  
-         
+      <div>
+      <div className="Portfolio">
+        <h1>Portfolio</h1>
+      <br></br>
+      <Container fluid>
       <Row>
         <Col size="md-6 sm-12">
           <CardProjectOne />
@@ -32,22 +29,21 @@ const Portfolio = () => {
           <CardProjectThree />
         </Col>
         <Col size="md-6 sm-12">
-          <CardProjectFour />
+          <CardProjectThree />
         </Col>
       </Row>
       <br></br>
       <Row>
         <Col size="md-6 sm-12">
-          <CardProjectFive />
+          <CardProjectOne />
         </Col>
         <Col size="md-6 sm-12">
-          <CardProjectSix />
+          <CardProjectTwo />
         </Col>
       </Row>
-      <br></br>
-      <br></br>
-      <Footer />
     </Container>
+    </div>
+    </div>
   );
   };
   
