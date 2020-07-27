ccabe46903520aece13075b1aef03e475bdc6a4b
Azucena Herrera <azucenaphx@gmail.com>
2020-07-27 09:48:15 -0700

    workinge page with centered elements

--- suzy-portfolio/src/App.js
+++ suzy-portfolio/src/App.js
@@ -5,18 +5,22 @@ import Navbar from "./components/Navbar";
 import Contact from "./pages/Contact"; 
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
+import { Container } from './components/Grid';
 
 function App() {  
     return (
       <Router>
       <div>      
         <Navbar />
+      <Container> 
         <Switch>
+
           <Route exact path="/" component={Home} />
           <Route exact path="/home" component={Home} />
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/contact" component={Contact} />
-        </Switch>          
+        </Switch>    
+        </Container>      
       </div>
     </Router>
     );
